dd2d34fa529d357ebe27defc6982418f47e22d8d
daffaazhar <daffaazharsda@gmail.com>
2023-11-15 21:53:18 +0700

    feat: slicing navbar

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -6,29 +6,25 @@ use App\Models\Candidate;
 use App\Models\Vote;
 use Illuminate\Http\Request;
 
-class CandidateController extends Controller
-{
+class CandidateController extends Controller {
     private Candidate $candidate;
     private  Vote $vote;
 
-
-    public function __construct(Candidate $candidate, Vote $vote)
-    {
+    public function __construct(Candidate $candidate, Vote $vote) {
         $this->candidate = $candidate;
         $this->vote = $vote;
     }
+
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
-    {
-        try{
-            return view("index", [
-                "candidate" => $this->candidate->all(),
-                "vote" => $this->vote->all(),
-            ]);
-        }
-        catch(\Exception $e){
+    public function index(Request $request) {
+        try {
+            $candidates = $this->candidate->all();
+            $votes = $this->vote->all();
+
+            return view("index", compact('candidates', 'votes'));
+        } catch (\Exception $e) {
             return redirect()->back()->with("error", $e->getMessage());
         }
     }
@@ -36,48 +32,42 @@ class CandidateController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
+    public function show(string $id) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
+    public function edit(string $id) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
+    public function update(Request $request, string $id) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
+    public function destroy(string $id) {
         //
     }
 }
